d15ef5f35a5eb18d792b23d3c0863c2cd9474a55
Johannes Kröger <johannes.kroeger@wheregroup.com>
2023-07-03 15:28:59 +0200

    Remove broken function to migration plugin *options*
    There is no standard or rules that provide a safe way to identify
    plugins' options...
    ref: https://hannes.enjoys.it/blog/2023/07/plugins-in-qgis3-ini/

--- datasources/Plugins/plugin_importer.py
+++ datasources/Plugins/plugin_importer.py
@@ -14,8 +14,6 @@ class PluginImporter:
         self.parser.optionxform = str
         self.source_qgis_ini_file = ""
         self.target_qgis_ini_file = ""
-        self.checked_items = []
-        self.plugin_list_widget = self.profile_manager.dlg.list_plugins
 
     def import_selected_plugins(self):
         """Copies selected plugins into target profile.
@@ -29,10 +27,8 @@ class PluginImporter:
         if not self.parser.has_section("PythonPlugins"):
             self.parser["PythonPlugins"] = {}
 
-        self.plugin_list_widget = self.profile_manager.dlg.list_plugins
-        for item in self.plugin_list_widget.findItems("", Qt.MatchContains | Qt.MatchRecursive):
+        for item in self.profile_manager.dlg.list_plugins.findItems("", Qt.MatchContains | Qt.MatchRecursive):
             if item.checkState() == Qt.Checked:
-                self.checked_items.append(item.text())
                 self.parser.set("PythonPlugins", item.text(), "true")
 
                 profile_paths = self.profile_manager.get_profile_paths()
@@ -52,33 +48,6 @@ class PluginImporter:
 
         with open(self.target_qgis_ini_file, 'w') as qgisconf:
             self.parser.write(qgisconf)
-
-        self.import_plugin_settings()
-
-    def import_plugin_settings(self):
-        """Gets plugin options from ini file and pastes them in target ini file"""
-        self.parser.clear()
-        self.parser.read(self.source_qgis_ini_file)
-
-        for item in self.checked_items:
-            get_plugin_options = []
-
-            if not self.parser.has_section(item):
-                continue
-            else:
-                get_plugin_options.append(self.parser.items(item))
-
-            self.parser.clear()
-            self.parser.read(self.target_qgis_ini_file)
-
-            if not self.parser.has_section(item):
-                self.parser[item] = {}
-
-            for option in get_plugin_options[0]:
-                self.parser.set(item, option[0], option[1])
-
-            with open(self.target_qgis_ini_file, 'w') as qgisconf:
-                self.parser.write(qgisconf)
 
     def set_ini_paths(self, source, target):
         self.source_qgis_ini_file = source
