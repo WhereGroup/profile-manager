eaf9d1fc3bf693d0b5f7fe9669345a4c215eab78
Johannes Kröger <johannes.kroeger@wheregroup.com>
2023-07-19 16:00:54 +0200

    Fix typo, add important TODO/FIXME

--- datasources/Bookmarks/bookmark_handler.py
+++ datasources/Bookmarks/bookmark_handler.py
@@ -72,10 +72,13 @@ class BookmarkHandler:
             .write(self.target_bookmark_file, pretty_print=True, encoding='utf-8', xml_declaration=True)
 
     @staticmethod
-    def remove_duplicates(source_toot_tag, target_tree, target_tree_root):
-        """Removes duplicate entries"""
+    def remove_duplicates(source_root_tag, target_tree, target_tree_root):
+        """Removes bookmarks from target that exist in the (to be imported) source too."""
+        # TODO FIXME this only checks the name of the bookmarks which will lead to false positives
+        #            it is ok and supported by QGIS to have the same name for multiple bookmarks
+        #            TODO compare the complete content of the xml node!
         target_root_tag = target_tree.findall('Bookmark')
-        for s_element in source_toot_tag:
+        for s_element in source_root_tag:
             for t_element in target_root_tag:
                 if s_element.attrib["name"] == t_element.attrib["name"]:
                     target_tree_root.remove(t_element)
